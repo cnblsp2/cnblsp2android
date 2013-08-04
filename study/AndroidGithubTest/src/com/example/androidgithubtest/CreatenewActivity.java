58a01caedcc29a63d6b8b26b4c71201a7cde0268
cnblsp2 <cnblsp2@gmail.com>
2013-08-04 12:54:49 +0800

    test add

---
+++ study/AndroidGithubTest/src/com/example/androidgithubtest/CreatenewActivity.java
@@ -0,0 +1,14 @@
+package com.example.androidgithubtest;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CreatenewActivity extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+
+}

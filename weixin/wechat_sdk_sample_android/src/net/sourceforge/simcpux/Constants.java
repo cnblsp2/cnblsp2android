7696b5c91e2d44055e0ad9a0a1e488f955ed36b6
cnblsp2 <cnblsp2@gmail.com>
2014-03-21 20:20:14 +0800

    commit
    commit

--- weixin/wechat_sdk_sample_android/src/net/sourceforge/simcpux/Constants.java
+++ weixin/wechat_sdk_sample_android/src/net/sourceforge/simcpux/Constants.java
@@ -2,7 +2,7 @@ package net.sourceforge.simcpux;
 
 public class Constants {
 	// APP_ID 替换为你的应用从官方网站申请到的合法appId
-    public static final String APP_ID = "wxd930ea5d5a258f4f";
+    public static final String APP_ID = "wx49c422103e44d632";
     
     public static class ShowMsgActivity {
 		public static final String STitle = "showmsg_title";
